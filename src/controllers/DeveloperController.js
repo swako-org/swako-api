e94470bf5b53dc0276e0397f56913ee3cebde8c0
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-04 20:03:06 -0300

    controllers: developer main route worked

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,11 +1,24 @@
 import Controller from '../structures/Controller';
 
+import DiscordRequest from '../utils/DiscordRequest';
+
+const FRONT_END_ROLE = '695439885578010634';
+const BACK_END_ROLE = '695370283141431447';
+
 class DeveloperController extends Controller {
   load(route) {
-    route.get('/', (req, res) => {
+    route.get('/', async (_, res) => {
+      const members = await DiscordRequest.getSwakoMembers().then(result =>
+        result.filter(member =>
+          member.roles.some(role =>
+            [FRONT_END_ROLE, BACK_END_ROLE].includes(role),
+          ),
+        ),
+      );
+
       return res.json({
-        front: [],
-        back: [],
+        frontEnd: members.filter(m => m.roles.includes(FRONT_END_ROLE)),
+        backEnd: members.filter(m => m.roles.includes(BACK_END_ROLE)),
       });
     });
   }
